17254068ed1f75388e92b1c6a637b1a29d813b84
nnoce14 <nnoce12@gmail.com>
2022-04-06 19:11:30 -0400

    FInished the write-to-me exercise in stream-adventure

--- stream-adventure/write-to-me.js
+++ stream-adventure/write-to-me.js
@@ -1,3 +1,12 @@
+const fs = require('fs')
 const { Writable } = require('stream')
 
-class WritableStream extends Writable {}
+class WritableStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log('writing:', chunk.toString())
+        callback()
+    }
+}
+
+const stream = new WritableStream()
+process.stdin.pipe(stream)
