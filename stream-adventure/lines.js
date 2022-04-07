864add07d57b9f34d4c21f5cbf7df928e0ebaf97
nnoce14 <nnoce12@gmail.com>
2022-04-07 17:16:55 -0400

    Completed the lines exercise in stream-adventure

---
+++ stream-adventure/lines.js
@@ -0,0 +1,12 @@
+const through = require('through2')
+const split2 = require('split2')
+
+let lineCount = 0;
+const stream = through(function (buf, _, next) {
+    const line = buf.toString()
+    this.push(lineCount % 2 === 0 ? line.toLowerCase() + '\n' : line.toUpperCase() + '\n')
+    lineCount++
+    next()
+})
+
+process.stdin.pipe(split2()).pipe(stream).pipe(process.stdout)
