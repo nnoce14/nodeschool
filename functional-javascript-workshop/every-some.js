8bde576a66800c561ad84e697ca63300a0cbdc5f
nnoce14 <nnoce12@gmail.com>
2022-04-09 18:12:06 -0400

    Completed the basic every-some exercise

---
+++ functional-javascript-workshop/every-some.js
@@ -0,0 +1,7 @@
+function checkUsersValid(goodUsers) {
+    return function allUsersValid(submittedUsers) {
+            return submittedUsers.every(submittedUser => (goodUsers.some(goodUser => goodUser.id == submittedUser.id)))
+    }
+}
+
+module.exports = checkUsersValid
