f3a4064e7ccb80bbe7a9e2836bdbaa62d471857d
nnoce14 <nnoce12@gmail.com>
2022-04-17 11:25:53 -0400

    Completed the partial application without bind exercise

---
+++ functional-javascript-workshop/partial-application-without-bind.js
@@ -0,0 +1,10 @@
+var slice = Array.prototype.slice
+
+function logger(namespace) {
+    return function logWithNamespace() {
+        var message = slice.call(arguments).join(' ')
+        console.log(namespace + " " + message)
+    }
+}
+
+module.exports = logger
