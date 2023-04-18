44c7d44248cafa7f2eed807b2a7988a8899e4084
nnoce14 <nnoce12@gmail.com>
2023-04-17 21:41:10 -0400

    pug

---
+++ expressworks/pug.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
+
+app.set('view engine', 'pug');
+
+app.get('/home', (r_, res) => {
+    res.render('index', {date: new Date().toDateString()});
+});
+
+app.listen(process.argv[2]);
